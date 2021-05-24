97ac4f8acbf42d436118ad49780aa97f8ee738e9
Tony Arteaga <tony.arteaga27@gmail.com>
2021-05-24 07:56:52 -0300

    Added costume recipe detail view.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -38,3 +38,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'ingredient', 'tags', 'time_minutes',
                   'price', 'link')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredient = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
